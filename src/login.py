006bbb055d672c384de6f0d400e1251b5499ebdd
surikbs <surikbs@gmail.com>
2023-10-25 01:47:10 +0530

    changes

--- src/login.py
+++
@@ -1,7 +0,0 @@
-
-
-async def get_access_token():
-    # Make a POST request to Zerodha's OAuth endpoint to get the access token
-    # You need to handle OAuth authentication and token retrieval here
-    access_token = "your_access_token"
-    return access_token
